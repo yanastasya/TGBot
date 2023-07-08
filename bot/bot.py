84ae9721bd8e521a9e33f4ffd5e2fb3da6444d78
KlincovaAnastasiya <aklin-93@yandex.ru>
2023-07-08 22:50:07 +0300

    пробую подключить redis

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
@@ -7,6 +8,7 @@ from config_data.config import Config, load_config
 from config_data.set_menu import set_main_menu
 from handlers import user_handlers, admin_handlers
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage, Redis
 
 from database.database import DataBase
 
@@ -30,13 +32,18 @@ async def main():
     config: Config = load_config()
 
     # Инициализируем хранилище (создаем экземпляр класса MemoryStorage)Вместо него нужно Redis
-    #redis: Redis = Redis(host='localhost')
-    #storage: RedisStorage = RedisStorage(redis=redis)
+    redis: Redis = Redis(
+        host=os.getenv('REDIS_HOST'),
+        port=os.getenv('REDIS_PORT'),
+        password=os.getenv('REDIS_PASSWORD'),
+        db=5
+    )
+    storage: RedisStorage = RedisStorage(redis=redis)
     
     db : DataBase = DataBase()  
     db.create_table_statistic()
     
-    storage: MemoryStorage = MemoryStorage()
+    #storage: MemoryStorage = MemoryStorage()
 
     bot: Bot = Bot(token=config.tg_bot.token,
                    parse_mode='HTML')
